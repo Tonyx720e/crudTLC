e26f410c7f4af4a752a12ed461d6c03979202934
Tony Medina <cayapadeveloper@gmail.com>
2024-11-06 22:33:49 +0100

    refactorizacion

--- crudTLC/src/main/java/datos/Conexion.java
+++ crudTLC/src/main/java/datos/Conexion.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package datos;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  *
  * @author tonym
  */
 public class Conexion {
+    private static final String JDBC_URL = "jdbc:mysql://localhost";
+    private static final String BASE_DATOS = "appmvcmodulo3";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+    private static final String URL_KEY = "?useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String URL_FINAL = JDBC_URL + "/" + BASE_DATOS + URL_KEY;
+    private static  BasicDataSource dataSource; 
+    
     
 }
