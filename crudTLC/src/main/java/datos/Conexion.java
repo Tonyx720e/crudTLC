dec7c4189bd760052d10c6ff2d0b02fd9a7b46cc
Tony Medina <cayapadeveloper@gmail.com>
2024-11-07 10:08:11 +0100

    crear conexion

--- crudTLC/src/main/java/datos/Conexion.java
+++ crudTLC/src/main/java/datos/Conexion.java
@@ -1,6 +1,11 @@
 
 package datos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
@@ -16,5 +21,40 @@ public class Conexion {
     private static final String URL_FINAL = JDBC_URL + "/" + BASE_DATOS + URL_KEY;
     private static  BasicDataSource dataSource; 
     
+    public static DataSource getDataSource(){
+    if(dataSource != null){
+    dataSource.setUrl(URL_FINAL);
+    dataSource.setUsername(USER);
+    dataSource.setPassword(PASS);
+    dataSource.setInitialSize(50);
+        }
+    return dataSource;
+    }
     
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt) {
+        try {
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+    public static void close(Connection conn) {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
 }
