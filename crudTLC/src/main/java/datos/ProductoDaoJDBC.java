82acfe16e07c8295c67a1de14e4070eaaa133787
maria cristina holguin salinas <mcholguinsalinas@gmail.com>
2024-11-11 01:25:25 +0100

    creacion de consultas en ProductoDaoJDBC.java

--- crudTLC/src/main/java/datos/ProductoDaoJDBC.java
+++ crudTLC/src/main/java/datos/ProductoDaoJDBC.java
@@ -1,5 +1,21 @@
 package datos;
 
 public class ProductoDaoJDBC {
-    
-}
+    private static final String SELECT = "SELECT idproducto,nombre FROM producto;";
+    private static final String INSERT = "INSERT INTO producto(nombre) values(?);";
+    private static final String UPDATE = "UPDATE `producto` SET `nombre` = ? WHERE (`idproducto` = ?)";
+    private static final String DELETE = "DELETE FROM `appmvcmodulo3`.`producto` WHERE (`idproducto` = ?)";
+
+    }
+
+
+
+
+
+
+
+
+  
+
+
+
